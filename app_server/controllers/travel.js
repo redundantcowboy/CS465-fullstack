f8f38a3788fabde440236040c4f91d447fd255be
Leanna Walker <leanna.walker1@snhu.edu>
2026-07-15 16:45:25 -0500

    Module 3 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,9 +1,14 @@
-/* GET travel view */
+// Get the file system to read JSON file 
+var fs = require('fs');
 
+// Read the trips data from the JSON file
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
+/* GET travel view */
 /* Added code based on instructions to control what happens when 
 someone goes to /travel */
 const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways'});
+    res.render('travel', { title: 'Travlr Getaways', trips});
 };
 
 module.exports = {
