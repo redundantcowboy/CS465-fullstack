5375662b38ba1d5a0bcbcad168281312e259ac2d
Leanna Walker <leanna.walker1@snhu.edu>
2026-08-20 14:11:19 -0500

    Module 6 completed baseline

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -6,11 +6,15 @@ const tripsController = require("../controllers/trips");
 router
     .route('/trips')
     // GET method routes tripList
-    .get(tripsController.tripsList);
+    .get(tripsController.tripsList)
+
+     // POST method adds a trip
+    .post(tripsController.tripsAddTrip);
 
 // GET Method routes tripsFindByCode - requires parameter
 router
     .route('/trips/:tripCode')
-    .get(tripsController.tripsFindByCode);
+    .get(tripsController.tripsFindByCode)
+    .put(tripsController.tripsUpdateTrip);
 
 module.exports = router;
